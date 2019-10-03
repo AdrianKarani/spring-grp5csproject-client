07f98ec6b9053a2356d89e65f45b0fdd45787b8b
Adrian Karani <karanimuturi@gmail.com>
2019-10-03 13:26:58 +0300

    Initial commit

---
+++ src/main/java/com/example/springgrp5csprojectclient/SpringGrp5csprojectClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgrp5csprojectclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGrp5csprojectClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGrp5csprojectClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springgrp5csprojectclient/SpringGrp5csprojectClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springgrp5csprojectclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringGrp5csprojectClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
