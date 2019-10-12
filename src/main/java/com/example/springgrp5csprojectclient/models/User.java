37c92680318a4241eb3e8efdf371626e1ef5fc46
irungu-ian <ian.irungu716@strathmore.edu>
2019-10-12 16:55:03 +0300

    Renamed Client model to User

--- src/main/java/com/example/springgrp5csprojectclient/models/User.java
+++ src/main/java/com/example/springgrp5csprojectclient/models/User.java
@@ -1,4 +1,4 @@
 package com.example.springgrp5csprojectclient.models;
 
-public class Client {
+public class User {
 }
--- src/test/java/com/example/springgrp5csprojectclient/SpringGrp5CsprojectUserApplicationTests.java
+++ src/test/java/com/example/springgrp5csprojectclient/SpringGrp5CsprojectUserApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringGrp5csprojectClientApplicationTests {
+public class SpringGrp5CsprojectUserApplicationTests {
 
     @Test
     public void contextLoads() {
